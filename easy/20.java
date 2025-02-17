cbda15578fa4d8a710e4021c7ab6542ff110a28e
vuphuong1794 <phuongvu1794@gmail.com>
2025-02-17 09:29:55 +0700

    update

---
+++ easy/20.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack();
+
+        for(char c : s.toCharArray()){
+            if(c == '(' || c == '{' || c == '['){
+                st.push(c);
+            }
+            else{
+                if(st.empty()){
+                    return false;
+                }
+                if(c == ')'){
+                    if(st.pop() != '('){
+                        return false;
+                    }
+                }
+                if(c == '}'){
+                    if(st.pop() != '{'){
+                        return false;
+                    }
+                }
+                if(c == ']'){
+                    if(st.pop() != '['){
+                        return false;
+                    }
+                }
+                
+
+            }
+        }
+        return st.empty();
+    }
+}
